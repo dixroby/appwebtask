66ae034e14e0d14163160d37f850e530fc585b82
dixroby arone cuellar <alestilodefe@gmail.com>
2020-11-01 01:24:07 -0500

    Refactor: Update enpoint get task.
    Se modifico el endpoint, de tal manera que devuelba las tareas como tal.

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -4,11 +4,11 @@ var mongoose = require('mongoose'),
 Task = mongoose.model('Tasks');
 
 
-exports.list_all_tasks = function(req, res) {
-  Task.find({}, function(err, task) {
+exports.list_all_tasks = async function(req, res) {
+  tsk = await Task.find({}, function(err, task) {
     if (err)
       res.send(err);
-    res.json({task});
+    res.json({tsk});
   });
 };
 
