bbdaefe7e8490fc758bc8abdc89f2773ad65a04c
dixroby arone cuellar <alestilodefe@gmail.com>
2020-11-01 00:53:04 -0500

    Feat: Add end point task whitch conection MongoDB.
    En esta parte se agrega end point  de task, con una conexión a MongoDB.

---
+++ api/controllers/todoListController.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+Task = mongoose.model('Tasks');
+
+
+exports.list_all_tasks = function(req, res) {
+  Task.find({}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({task});
+  });
+};
+
+exports.create_a_task = function(req, res) {
+  const dbTask=req.body;
+  Task.create(dbTask,(err,data)=>{
+        if (err) {
+            console.log("error 500");
+            res.status(500).send(err);
+        } else {
+            console.log("201 ok enviado");
+            res.status(201).send({message: "se creo correctamente la tarea",data});
+        }
+    });
+};
+
+exports.read_a_task = function(req, res) {
+  Task.findById(req.params.taskId, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({task});
+  });
+};
+
+exports.update_a_task = function(req, res) {
+  Task.findOneAndUpdate({_id:req.params.taskId}, req.body, {new: true}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({task});
+  });
+};
+// Task.remove({}).exec(function(){});
+exports.delete_a_task = function(req, res) {
+
+  Task.remove({
+    _id: req.params.taskId
+  }, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Task successfully deleted' });
+  });
+};
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 9000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'),
+  bodyParser = require('body-parser');
+const morgan = require('morgan');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+
+const connection_url ="mongodb+srv://"+process.env.MONGO_USER+":"+process.env.MONGO_PASSWORD+"@cluster0.urc6a.mongodb.net/"+process.env.MONGO_DATABASE+"?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+});
+
+app.use(morgan('dev'));
+app.use(express.json())
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*"),
+    res.setHeader("Access-Control-Allow-Headers","*"),
+    next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+app.get('/',(req,res) => res.status(200).send("hola"));
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on localhost:' + port);
